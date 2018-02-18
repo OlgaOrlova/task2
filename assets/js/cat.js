fa3a97045e548c4a12c4f457e455824ac8b0365f
OlgaOrlova <olgaviktorovnaorlova@gmail.com>
2018-02-18 17:59:32 +0200

    impl

---
+++ assets/js/cat.js
@@ -0,0 +1,5 @@
+export default class Cat {
+    sayHi() {
+        alert("Meow!");
+    }
+}
---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+import greet from './greeter.js';
+import Cat from './cat.js';
+
+console.log("I'm the entry point");
+new Cat().sayHi();
+greet();
